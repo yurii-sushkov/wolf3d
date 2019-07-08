adf18ce3729aec74aec3767663d21c1671bece64
Yurii Sushkov <s.supremus@gmail.com>
2019-07-08 15:22:06 +0300

    improved compile-linux.sh file

--- compile-linux.sh
+++ compile-linux.sh
@@ -1,2 +1,7 @@
+sudo apt install -y git gcc make
+sudo apt install --assume-yes libxext-dev libbsd-dev
+sudo cp minilibx/libmlx.a /usr/local/lib
+sudo cp minilibx/libmlx_Linux.a /usr/local/lib
+sudo cp minilibx/mlx.h /usr/local/include
 make -C libft
 gcc *.c -o wolf3d -L./libft -L./minilibx -lft -lm -lmlx -lXext -lX11 -lbsd
